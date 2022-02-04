20922c0ac373c2183c9b6737a6416a1bd8a14598
rici86 <rici86@yandex.com>
2022-02-04 18:08:42 +0100

    prova spostamento includes

---
+++ it/su-di-me.php
@@ -0,0 +1,18 @@
+<?php include 'include/head.php';?>
+
+<body>
+    <div id="main-container">
+        <?php include 'nav.php';?>
+
+        <main>
+            main content
+        </main>
+
+        <?php include 'footer.php';?>
+
+    </div>
+
+    <!-- <?php include 'end.php';?> -->
+
+</body>
+</html>
