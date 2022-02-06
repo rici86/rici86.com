dfac9ba274606570ce0cda9c2ccd2a14f827037c
rici86 <rici86@yandex.com>
2022-02-06 18:39:29 +0100

    inizio navbar

--- it/include/nav.php
+++ it/include/nav.php
@@ -1,5 +1,12 @@
 <nav>
-    <a href="index-eng.html" title="English">
+    <div class="navbar">
+        <a href="su-di-me.php" class="button button-main">Su di me</a>
+        <a href="sviluppo-web.php" class="button button-main">Sviluppo web</a>
+        <a href="eventi.php" class="button button-main">Eventi</a>
+        <a href="scrittura.php" class="button button-main">Scrittura</a>
+        <a href="fai-da-te.php" class="button button-main">Fai-da-te</a>
+    </div>
+    <a href="index-eng.html" title="English" class="">
         <img src="../src/img/eng.png" alt="English" class="lang">
     </a>
 </nav>
