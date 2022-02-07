8e6474724c291ef1203887a7ac7dbff937c84b6c
rici86 <rici86@yandex.com>
2022-02-07 10:40:13 +0100

    fix logo in navbar

--- it/include/nav.php
+++ it/include/nav.php
@@ -2,8 +2,10 @@
     $activePage = basename($_SERVER['PHP_SELF'], ".php");
 ?>
 <nav>
-    <img src="../src/img/logobianco.png" alt="Rici86" class="logo">
-    <div class="navbar">
+    <a href="../index.html" class="nav-logo">
+        <img src="../src/img/logobianco.png" alt="Rici86">
+    </a>
+    <div class="navbar" id="navigation">
         <a href="su-di-me.php" class="button button-main <?= (in_array($activePage, ['su-di-me'])) ? 'active':''; ?>">Su di me</a>
         <a href="sviluppo-web.php" class="button button-main">Sviluppo web</a>
         <a href="eventi.php" class="button button-main">Eventi</a>
