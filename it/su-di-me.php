3c0d53d9997a312cc76a4ea0399ba06e7893533d
rici86 <rici86@yandex.com>
2022-02-07 17:04:20 +0100

    inizio testo bio ita

--- it/su-di-me.php
+++ it/su-di-me.php
@@ -28,7 +28,99 @@
         <?php include 'include/nav.php';?>
 
         <main>
-            main content
+            <section class="main-row">
+                <div class="card">
+                    <h1 class="card-title">Qualcosa su di me</h1>
+                    <p>
+                        Benvenuto, navigante, nel mio piccolo porto del web. 
+                        <br>Se sei arrivato qui, suppongo tu voglia conoscermi un po' meglio. 
+                        <br>Mi presento, sono Sara, ma su internet mi sono sempre firmata Rici.
+                    </p>
+                    <p>
+                        <i>Professione:</i> Front-end developer in una software agency.
+                    </p>
+                    <p>
+                        Detto così fa scena, con i paroloni complicati in inglese che piacciono tanto nei CV.
+                    </p>
+                    <p>
+                        <i>Laurea:</i> Archeologia classica.
+                    </p>
+                    <p>
+                        Eh no, un momento, questo è strano. 
+                        <br>Niente laurea in informatica? Design? Ingegneria? Nessun esame di Analisi?
+                    </p>
+                    <p>
+                        Esatto, proprio così. 
+                        <br>Greco, latino e pratica di scavo.
+                    </p>
+                    <p>
+                        <i>Ma allora come sei finita a sviluppare in una software agency</i>, ti starai chiedendo.
+                    </p>
+                    <!-- <ol class="sottomenu">
+                        <li>
+                            <a href="#percorso">Il mio percorso verso lo sviluppo</a>                        
+                        </li>
+                        <li>
+                            <a href="#oggi">Il mio lavoro oggi</a>
+                        </li>
+                        <li>
+                            <a href="#archeo">E il mio passato "archeologico"?</a>
+                            <ol class="sottomenu">
+                                <li>
+                                    <a href="">Archeologia e musei</a>
+                                </li>
+                                <li>
+                                    <a href="">La mia vita in Irlanda</a>
+                                </li>
+                                <li>
+                                    <a href="">Turismo ed eventi</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ol> -->
+                </div>
+                <div class="images">
+                    <img src="../src/img/rici86-sara-rossi.jpg" alt="" class="img-responsive photo">
+                </div>
+            </section>
+
+            <section class="main-row" id="percorso">                
+                <div class="images"></div>
+                <div class="card">
+                    <h2 class="card-title">Il mio percorso verso lo sviluppo </h2>
+                    <p>
+                        Prima da autodidatta, aprendo il "cofano" dei siti fatti da altri per annusare cosa c'era dentro, dietro, sotto. Alla ricerca del motore che faceva funzionare le cose.
+                    </p>
+                    <p>
+                        Poi trovando corsi, mano a mano che la mia voglia di imparare e di specializzarmi cresceva.
+                    </p>
+                    <p>
+                        Ho fatto tanta esperienza sul campo: ho iniziato subito con piccoli lavoretti, anche durante l'università, per capire poi che era questo il mondo che volevo davvero, la professione giusta per me.
+                    </p>
+                    <p>
+                        E così ho trovato persone più esperte con cui lavorare, per cui sviluppare piccoli progetti. Ho messo da parte avidamente ogni cosa che potevo imparare.
+                    </p>
+                    <p>
+                        Ho seguito corsi in aula, poi ho scoperto la potenza vera delle community sul web: siti specializzati, newsletter di aggiornamento, corsi online di approfondimento, tutorial per tutti i livelli.
+                    </p>
+                    <p>
+                        Dopo diverse candidature e colloqui, mi hanno assunta. Per fare questo lavoro.                    
+                        <br>Prima una startup, poi una web agency, poi una casa editrice, infine la software agency dove ora faccio il lavoro che sognavo.
+                    </p>
+                </div>
+            </section>
+
+            <section class="main-row" id="oggi">
+                <div class="card">
+                    <h2 class="card-title">Il mio lavoro oggi</h2>
+                </div>
+            </section>
+            
+            <section class="main-row" id="archeo">
+                <div class="card">
+                    <h2 class="card-title">E il mio passato "archeologico"?</h2>
+                </div>
+            </section>
         </main>
 
         <?php include 'include/footer.php';?>
