42bbd94e17fb45fa308b0f5a5b5549472a022d93
rici86 <rici86@yandex.com>
2022-02-07 17:49:41 +0100

    mobile menu

--- it/include/nav.php
+++ it/include/nav.php
@@ -11,16 +11,20 @@
         <a href="eventi.php" class="button button-main">Eventi</a>
         <a href="scrittura.php" class="button button-main">Scrittura</a>
         <a href="fai-da-te.php" class="button button-main">Fai-da-te</a>
-        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-            <i class="fa fa-bars"></i>
-        </a>
     </div>
     
-    <!-- <div class="navbar-toggler">
+    <div class="navbar-toggler" onclick="mobileToggle()">
         <i class="fas fa-bars"></i>
-    </div> -->
+    </div>
 
     <a href="index-eng.html" title="English" class="">
         <img src="../src/img/eng.png" alt="English" class="lang">
     </a>
 </nav>
+
+<script>
+    function mobileToggle() {
+  var element = document.getElementById("navigation");
+  element.classList.toggle("show");
+}
+</script>
