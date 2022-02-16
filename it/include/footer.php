4f2b19534e554a21cd3a2199bc72410eade1f685
rici86 <rici86@yandex.com>
2022-02-16 18:11:07 +0100

    footer

--- it/include/footer.php
+++ it/include/footer.php
@@ -1,8 +1,8 @@
 <footer>
     <div class="footer-container">
         <div class="logo">
-            <a href="index-eng.html" title="English">
-                <img src="src/img/eng.png" alt="English" class="lang">
+            <a href="index.html" title="Home page">
+                <img src="../src/img/logo-esteso-bianco.png" alt="Rici86" class="logo">
             </a>
         </div>
         <div class="social">
