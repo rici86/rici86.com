4b40897875d15420128a06c32430a6e181ffe5b8
rici86 <rici86@yandex.com>
2022-02-17 17:56:28 +0100

    inizio pagina sviluppo

---
+++ it/sviluppo-web.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Il mio portfolio web | Coder, Crafter, Creative </title>
+
+    <!-- Meta tags  -->
+    <meta name="description" content="Front-end developer per lavoro, crafter e Irish dance nerd per passione" />
+	
+    <meta property="article:author" content="https://www.facebook.com/rici86" />
+    <meta property="og:title" content="Rici86 - Coder, Crafter, Creative" />
+    <meta property="og:description" content="Front-end developer per lavoro, crafter e Irish dance nerd per passione" />
+    <meta property="og:site_name" content="Rici86" />
+    <meta property="og:image" content="../src/img/spirit-of-the-forest.jpg" />
+	
+	<link rel="shortcut icon" href="../src/img/logofavicon.png" type="imag/png" />
+
+    <!-- Css  -->
+    <link href="../src/css/main.css" rel="stylesheet">
+    <link href="../src/fontawesome/css/all.css" rel="stylesheet">
+</head>
+
+<body>
+    <div id="main-container">
+        <?php require_once 'include/nav.php';?>
+
+        <a href="../en/about.php" title="English" class="lang-switcher">
+            <img src="../src/img/eng.png" alt="English" class="lang">
+        </a>
+
+        <main class="with-shape-divider">
+            <section class="main-row">
+                <div class="card">
+                    <h1 class="card-title">Siti web</h1>
+                    <p>
+                        
+                    </p>
+                </div>
+                <div class="images">
+                    
+                </div>
+            </section>
+            
+            <?php require_once '../include/shape-divider.php';?>
+
+        </main>
+
+        <?php require_once 'include/footer.php';?>
+
+    </div>
+
+    <!-- <?php require_once 'include/end.php';?> -->
+
+</body>
+</html>
