63db3120683df5924e432ec8ac33d00fb447d0ab
rici86 <rici86@yandex.com>
2022-03-02 15:17:04 +0100

    GA4

--- include/analytics.php
+++ include/analytics.php
@@ -1,9 +1,9 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-27372610-1"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-Q799L3J1KZ"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'UA-27372610-1');
+  gtag('config', 'G-Q799L3J1KZ');
 </script>
