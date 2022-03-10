bc06d5b683f5e6f5cecf2462b3c8b27fdced9e2b
rici86 <rici86@yandex.com>
2022-03-10 15:40:09 +0100

    slider crafts as include

---
+++ include/slider-crafts.php
@@ -0,0 +1,11 @@
+<div class="slider-siti">
+    <div class="box-sito">
+        <img src="../src/img/crafts/sewing-pumpkins.jpg" alt="" class="img-responsive">
+        <div class="sito-description">
+            <div>
+                <p class="sito-titolo">Zucche in stoffa</p>
+                <p class="sito-date">Cucito</p>
+            </div>                                
+        </div>
+    </div>
+</div>
--- it/fai-da-te.php
+++ it/fai-da-te.php
@@ -63,18 +63,7 @@
                     </div>
                 </div>
                 <div class="">
-                    <div class="slider-siti">
-                        <div class="box-sito">
-                            <img src="../src/img/crafts/sewing-pumpkins.jpg" alt="" class="img-responsive">
-                            <div class="sito-description">
-                                <div>
-                                    <p class="sito-titolo">Zucche in stoffa</p>
-                                    <p class="sito-date">Cucito</p>
-                                </div>                                
-                            </div>
-                        </div>
-                    </div>
-                    
+                    <?php require_once '../include/slider-crafts.php';?>                    
                 </div>
             </section>
 
