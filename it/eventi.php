45e156fe9621dc60d0dfeb954c665089ae3817ba
rici86 <rici86@yandex.com>
2022-03-17 14:02:04 +0100

    pagina eventi

---
+++ it/eventi.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Organizzazione eventi | Rici86: Coder, Crafter, Creative </title>
+
+    <!-- Meta tags  -->
+    <meta name="description" content="Front-end developer per lavoro, crafter e Irish dance nerd per passione" />
+	
+    <meta property="article:author" content="https://www.facebook.com/rici86" />
+    <meta property="og:title" content="Rici86 - Coder, Crafter, Creative" />
+    <meta property="og:description" content="Front-end developer per lavoro, crafter e Irish dance nerd per passione" />
+    <meta property="og:site_name" content="Rici86" />
+    <meta property="og:image" content="../src/img/spirit-of-the-forest.jpg" />
+	
+	<link rel="shortcut icon" href="../src/img/logofavicon.png" type="imag/png" />
+
+    <!-- Css  -->
+    <link href="../src/css/main.css?v=1.11" rel="stylesheet">
+    <link href="../src/fontawesome/css/all.css" rel="stylesheet">
+
+    <?php require_once '../include/analytics.php';?>
+</head>
+
+<body>
+    <div id="main-container">
+        <?php require_once 'include/nav.php';?>
+
+        <a href="../en/events.php" title="English" class="lang-switcher">
+            <img src="../src/img/eng.png" alt="English" class="lang">
+        </a>
+
+        <main class="with-shape-divider">
+            <section class="main-row">
+                <div class="card">
+                    <h1 class="card-title">Come ho iniziato a organizzare eventi: le rievocazioni</h1>
+                    <p>
+                        Ho iniziato a organizzare eventi quasi per caso. 
+                        <br>I primi sono stati nel 2009, il primo concerto solista di mia madre e una mostra al Museo di cui ero diventata direttore da pochi mesi. 
+                    </p>
+                    <p>
+                        Nel 2012, la prima rievocazione storica medievale. Fino a quel momento avevo sempre partecipato alle Giornate Medievali di Zavattarello come accompagnatore museale, entravo nell'atmosfera della festa presentandomi al pubblico con un abito a tema. 
+                        <br>A tre settimane dall'evento di quell'agosto, tutto da rifare: tocca a me riorganizzare gli spettacoli da zero, su due piedi. 
+                        <br>Avendo diversi amici e conoscenti all'interno di gruppi di rievocazione, sono riuscita a trovare rievocatori disponibili che ci hanno permesso di salvare la festa. 
+                    </p>
+                    <p>
+                        Da quel momento è iniziata la mia passione per l'organizzazione di eventi.
+                        <br>Mi sono occupata di organizzare, gestire e coordinare gli spettacoli delle Giornate Medievali fino al 2019. È stato uno degli eventi più faticosi e impegnativi, ma soprattutto ricco di soddisfazioni. 
+                        <br>In famiglia abbiamo anche la passione per la rievocazione, quindi è stato un attimo coinvolgere i miei familiari. Abbiamo contemporaneamente affinato la nostra storicità come rievocatori e offerto al pubblico un evento sempre più gradito. 
+                    </p>
+                    
+                </div>
+                <figure class="">
+                    <img src="../src/img/foto/2019-gmzava01-mbr.jpg" alt="" class="img-responsive photo">
+                    <figcaption>Foto by MBR Photography</figcaption>
+                </figure>
+            </section>
+
+            <section class="main-row main-row-invert">
+                <div class="">
+                    <img src="../src/img/foto/2013-gttriora.jpg" alt="" class="img-responsive photo">
+                </div>
+                <div class="card">
+                    <h1 class="card-title">I Ghost Tour di Autunnonero</h1>
+                    <p>
+                        Dalla prima rievocazione è stato un attimo. 
+                        <br>L'anno dopo sono stata coinvolta in un progetto che ho adorato: i Ghost Tour di Autunnonero in Liguria. Prima a Trioria, il borgo delle streghe, e poi a Dolceacqua. 
+                    </p>
+                    <p>
+                        Sono eventi speciali e suggestivi, in cui gli storyteller narrano fatti storici tragici e misteriosi, storie di fantasmi, folklore e leggende. 
+                        <br>Una grande ricerca storica alle spalle consente ai Ghost Tour una grande accuratezza storica, che fa da sfondo a questi percorsi nel mistero, ricchi di emozioni.
+                    </p>
+                    <p>
+                        Ho iniziato a vestire i panni di storyteller in occasione di un Ghost Tour privato in lingua inglese. Mi sono subito innamorata del copione, del luogo, dell'atmosfera, dello staff straordinario con cui ho lavorato. 
+                        <br>Ad ogni nuova edizione, rinnovavo la mia disponibilità. Ho affinato le mie abilità recitative per dare il meglio: lavorare con attori professionisti mi ha insegnato molto, ed è stato veramente lusinghiero potermi confrontare con loro. 
+                    </p>
+                    <p>
+                        Non solo recitazione, ho anche contribuito alla promozione e all'organizzazione di diverse edizioni. 
+                    </p>                    
+                </div>
+            </section>
+                        
+            <?php require_once '../include/shape-divider.php';?>
+
+        </main>
+
+        <?php require_once 'include/footer.php';?>
+
+    </div>
+
+    <!-- <?php require_once 'include/footer.php';?> -->
+
+</body>
+</html>
