658d24c30e12e650d9e059a494f24b217ca26edf
rici86 <rici86@yandex.com>
2022-03-19 14:58:38 +0100

    inizio pagina Scritura

---
+++ it/scrittura.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Il piacere di scrivere | Rici86: Coder, Crafter, Creative </title>
+
+    <!-- Meta tags  -->
+    <meta name="description" content="Front-end developer per lavoro, crafter e Irish dance nerd per passione" />
+	
+    <meta property="article:author" content="https://www.facebook.com/rici86" />
+    <meta property="og:title" content="Rici86 - Coder, Crafter, Creative" />
+    <meta property="og:description" content="Front-end developer per lavoro, crafter e Irish dance nerd per passione" />
+    <meta property="og:site_name" content="Rici86" />
+    <meta property="og:image" content="../src/img/spirit-of-the-forest.jpg" />
+	
+	<link rel="shortcut icon" href="../src/img/logofavicon.png" type="imag/png" />
+
+    <!-- Css  -->
+    <link href="../src/css/main.css?v=1.11" rel="stylesheet">
+    <link href="../src/fontawesome/css/all.css" rel="stylesheet">
+
+    <?php require_once '../include/analytics.php';?>
+</head>
+
+<body>
+    <div id="main-container">
+        <?php require_once 'include/nav.php';?>
+
+        <a href="../en/writing.php" title="English" class="lang-switcher">
+            <img src="../src/img/eng.png" alt="English" class="lang">
+        </a>
+
+        <main class="with-shape-divider">
+            <section class="main-row" aria-labelledby="#secton-title-1">
+                <div class="card">
+                    <h1 class="card-title" id="secton-title-1">Una passione di lunga data per la parola scritta</h1>
+                    <p>
+                        Ho voluto imparare molto presto come interpretare quei segni che chiamiamo lettere, e <a href="https://rici86.blogspot.com/2014/11/storia-di-un-pesce-che-va-scoprire-il.html">da quando ho iniziato a scrivere</a> non ho mai smesso. 
+                        <br>Amo la sensazione che mi dà vedere una pagina bianca prendere vita, che si tratti di usare una penna o una tastiera. 
+                        <br>Mi è sempre piaciuto scrivere, e anche disegnare, liberare la mia fantasia e fissarla su carta.
+                    </p>
+                    <p>
+                        Il mio mezzo di espressione preferito mi ha portata anche ad alcune piccole pubblicazioni. 
+                        <br>Alcune di carattere scientifico: articoli per riviste specializzate in ambito archeologico o museologico. 
+                        <br>Altre, invece, di carattere letterario. 
+                    </p>                    
+                    <p>
+                        La mia passione per la scrittura trova ora sfogo anche sul web, in particolare all'interno del <a href="https://www.lrcrafts.it">blog #LRCrafts</a>, dedicato alla passione di famiglia per il fai-da-te. 
+                    </p>
+                    <p>
+                        Scrivere è una passione che richiede tempo, tranquillità e il giusto stato d'animo. Non sempre si concilia con lo stile di vita dinamico e ricco di interessi che ho scelto. 
+                        <br>La parola scritta, però, rimane una parte importante della mia quotidianità. 
+                    </p>
+                    <blockquote>
+                        Scrivere è viaggiare senza la seccatura dei bagagli.
+                        <br>(Emilio Salgari)</i>
+                    </blockquote>
+                </div>
+                <figure class="">
+                    <img src="../src/img/foto/2014-presentazionelibro.jpg" alt="" class="img-responsive photo">
+                    <figcaption>La presentazione del libro "Misteri e manieri d'Oltrepò" contenente un mio racconto</figcaption>
+                </figure>
+            </section>
+
+            <section class="main-row main-row-invert" aria-labelledby="#secton-title-2">
+                <div class="images books">
+                    <div class="book-container">
+                        <div class="book">
+                            <img
+                            alt="I racconti segreti della Lombardia"
+                            src="../src/img/lombardia-segreta-cover.jpg"
+                            />
+                        </div>
+                    </div>
+                    <div class="book-container">
+                        <div class="book">
+                            <img
+                            alt="Misteri e Manieri d'Oltrepò"
+                            src="../src/img/misteri-manieri-cover.webp"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <h1 class="card-title" id="secton-title-2">"Lia"</h1>
+                    <p>
+                        Un mio racconto è stato pubblicato in due diverse versioni all'interno di due raccolte. 
+                        <br><i>Lia</i> è un racconto di pura fantasia, ma che riprende i fatti storici legati alla figura di Pietro Dal Verme, signore di Zavattarello nel XV secolo. 
+                        <br>Un personaggio che conosco bene, e a cui ho dedicato anche <a href="misteri-del-castello.php">il copione di uno spettacolo teatrale</a>. 
+                    </p>
+                    <p>
+                        Lia è una giovane studentessa che si trova a svolgere un tirocinio nel Castello di #Zavattarello.
+                        <br>Le suggestive atmosfere delle antiche sale stuzzicano la sua fantasia e la portano a sognare spesso ad occhi aperti, fantasticando delle vicende e dei personaggi che sono passati nei secoli per le stanze e i corridoi della rocca. Ma una storia più di altre attira la sua curiosità: la leggenda del fantasma di Pietro dal Verme, morto avvelenato per mano della seconda moglie Chiara Sforza.
+                        <br>Tra leggenda e indagini dei ricercatori del paranormale, le fantasie di Lia si intensificano. Ma il confine tra suggestione e realtà è molto labile, specialmente immersi nei mille anni di storia di una fortezza medievale...
+                    </p>
+                </div>
+            </section>
+            
+                        
+            <?php require_once '../include/shape-divider.php';?>
+
+        </main>
+
+        <?php require_once 'include/footer.php';?>
+
+    </div>
+
+    <!-- <?php require_once 'include/footer.php';?> -->
+
+</body>
+</html>
