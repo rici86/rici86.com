f1cc6044f74861c145a35eabc12608c2ee451bf9
rici86 <rici86@yandex.com>
2022-03-24 17:27:23 +0100

    didascalie

--- include/slider-misteri.php
+++ include/slider-misteri.php
@@ -1,86 +1,100 @@
 <div class="slider-siti">
     <div class="box-sito">
         <img src="../src/img/misteri/misteri01.jpg" alt="" class="img-responsive">      
-        <div class="sito-description">
+        <div class="sito-description no-flex">            
             <p class="sito-url">L'atmosfera che accoglie il visitatore all'ingresso dell'evento</p>
+            <p class="sito-date">Foto di Angelo Cannella</p>            
         </div>  
     </div>
     <div class="box-sito">
         <img src="../src/img/misteri/misteri02.jpg" alt="" class="img-responsive"> 
-        <div class="sito-description">
+        <div class="sito-description no-flex">
             <p class="sito-url">Chiara Sforza trama vendetta</p>
+            <p class="sito-date">Foto di Angelo Cannella</p>
         </div>        
     </div>
     <div class="box-sito">
         <img src="../src/img/misteri/misteri03.jpg" alt="" class="img-responsive">        
-        <div class="sito-description">
+        <div class="sito-description no-flex">
             <p class="sito-url">Musica dal vivo con l'arpa di Claudia Murachelli</p>
+            <p class="sito-date">Foto di Angelo Cannella</p>            
         </div> 
     </div>
     <div class="box-sito">
         <img src="../src/img/misteri/misteri04.jpg" alt="" class="img-responsive">   
-        <div class="sito-description">
+        <div class="sito-description no-flex">
             <p class="sito-url">Pietro Dal Verme morì avvelenato il 17 ottobre 1485</p>
+            <p class="sito-date">Foto di Angelo Cannella</p>            
         </div>      
     </div>
     <div class="box-sito">
         <img src="../src/img/misteri/misteri05.jpg" alt="" class="img-responsive">     
-        <div class="sito-description">
+        <div class="sito-description no-flex">
             <p class="sito-url">Le celle medievali non erano certo luoghi piacevoli...</p>
+            <p class="sito-date">Foto di Angelo Cannella</p>            
         </div>    
     </div>
     <div class="box-sito">
         <img src="../src/img/misteri/misteri06.jpg" alt="" class="img-responsive">     
-        <div class="sito-description">
+        <div class="sito-description no-flex">
             <p class="sito-url">Una lettera annuncia alla corte di Milano la morte di Pietro Dal Verme</p>
+            <p class="sito-date">Foto di Angelo Cannella</p>            
         </div>    
     </div>
     <div class="box-sito">
         <img src="../src/img/misteri/misteri07.jpg" alt="" class="img-responsive">   
-        <div class="sito-description">
+        <div class="sito-description no-flex">
             <p class="sito-url">Tredici edizioni dal 2013 al 2019, ogni serata un sold-out</p>
+            <p class="sito-date">Foto di Angelo Cannella</p>            
         </div> 
     </div>
     <div class="box-sito">
         <img src="../src/img/misteri/misteri08.jpg" alt="" class="img-responsive"> 
-        <div class="sito-description">
+        <div class="sito-description no-flex">
             <p class="sito-url">Il viaggio giunge anche ai nostri giorni e racconta le indagini paranormali svoltesi realmente nel Castello di Zavattarello</p>
+            <p class="sito-date">Foto di Angelo Cannella</p>            
         </div>       
     </div>
     <div class="box-sito">
         <img src="../src/img/misteri/misteri09.jpg" alt="" class="img-responsive">        
-        <div class="sito-description">
+        <div class="sito-description no-flex">
             <p class="sito-url">Il Cavaliere Senza Tempo accompagna i viaggiatori nel loro passaggio dal XV secolo ai giorni nostri</p>
+            <p class="sito-date">Foto di Angelo Cannella</p>            
         </div>
     </div>
     <div class="box-sito">
         <img src="../src/img/misteri/misteri10.jpg" alt="" class="img-responsive"> 
-        <div class="sito-description">
+        <div class="sito-description no-flex">
             <p class="sito-url">Il personaggio più oscuro e misterioso di tutti: il fantasma di Pietro Dal Verme</p>
+            <p class="sito-date">Foto di Angelo Cannella</p>            
         </div>       
     </div>
     <div class="box-sito">
         <img src="../src/img/misteri/misteri11.jpg" alt="" class="img-responsive">        
-        <div class="sito-description">
+        <div class="sito-description no-flex">
             <p class="sito-url">Prigionieri, eretici, dannati o pazzi: al pubblico l'interpretazione</p>
+            <p class="sito-date">Foto di Angelo Cannella</p>            
         </div>
     </div>
     <div class="box-sito">
         <img src="../src/img/misteri/misteri12.jpg" alt="" class="img-responsive">        
-        <div class="sito-description">
+        <div class="sito-description no-flex">
             <p class="sito-url">Chiara Sforza organizza l'avvelenamento del marito Pietro Dal Verme</p>
+            <p class="sito-date">Foto di Angelo Cannella</p>            
         </div>
     </div>
     <div class="box-sito">
         <img src="../src/img/misteri/misteri13.jpg" alt="" class="img-responsive"> 
-        <div class="sito-description">
+        <div class="sito-description no-flex">
             <p class="sito-url">La ricercatrice del paranormale racconta l'episodio misterioso più famoso realmente accaduto: la storia della sedia spostata</p>
+            <p class="sito-date">Foto di Angelo Cannella</p>            
         </div>       
     </div>
     <div class="box-sito">
         <img src="../src/img/misteri/misteri14.jpg" alt="" class="img-responsive">        
-        <div class="sito-description">
+        <div class="sito-description no-flex">
             <p class="sito-url">L'araldo conduce il pubblico lungo il percorso</p>
+            <p class="sito-date">Foto di Angelo Cannella</p>            
         </div>
     </div>
 </div>
