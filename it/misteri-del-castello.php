4bcfb94e91533c7e1a1c9ba470eeaae4cf5b80ac
rici86 <rici86@yandex.com>
2022-03-24 15:49:25 +0100

    inizio pagina Misteri

---
+++ it/misteri-del-castello.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>I Misteri del Castello: il viaggio nel mistero del Castello Dal Verme di Zavattarello | Rici86: Coder, Crafter, Creative </title>
+
+    <!-- Meta tags  -->
+    <meta name="description" content="Front-end developer per lavoro, crafter e Irish dance nerd per passione" />
+	
+    <meta property="article:author" content="https://www.facebook.com/rici86" />
+    <meta property="og:title" content="Rici86 - Coder, Crafter, Creative" />
+    <meta property="og:description" content="Front-end developer per lavoro, crafter e Irish dance nerd per passione" />
+    <meta property="og:site_name" content="Rici86" />
+    <meta property="og:image" content="../src/img/spirit-of-the-forest.jpg" />
+	
+	<link rel="shortcut icon" href="../src/img/logofavicon.png" type="imag/png" />
+
+    <!-- Css  -->
+    <link href="../src/css/main.css?v=1.11" rel="stylesheet">
+    <link href="../src/fontawesome/css/all.css" rel="stylesheet">
+
+    <?php require_once '../include/analytics.php';?>
+</head>
+
+<body>
+    <div id="main-container">
+        <?php require_once 'include/nav.php';?>
+
+        <a href="../en/events.php" title="English" class="lang-switcher">
+            <img src="../src/img/eng.png" alt="English" class="lang">
+        </a>
+
+        <main class="with-shape-divider">
+            <section class="main-row" aria-labelledby="#secton-title-1">
+                <div class="card">
+                    <h1 class="card-title" id="secton-title-1">I Misteri del Castello</h1>
+                    <p>
+                        Dalle leggende agli strani avvenimenti, attraverso le indagini dei ricercatori del paranormale, il visitatore puà immergersi per una notte nei misteri del Castello di Zavattarello, legati a un ormai celebre assassinio.
+                        <br>Si narra che Pietro Dal Verme, avvelenato dalla sua seconda moglie Chiara Sforza il 17 ottobre 1485, sia la presenza che aleggia nelle sale del suo antico maniero.
+                        <br>Diversi fatti inspiegabili hanno avuto luogo all'interno del castello nel corso della sua lunga storia: proprio a partire da questi, i ricercatori del paranormale hanno intrapreso indagini di tipo scientifico per saperne di più. 
+                        <br>Durante questa particolare serata, i visitatori vengono condotti nello speciale <i>viaggio nel mistero</i>, per scoprire i luoghi protagonisti della leggenda e dei misteriosi avvenimenti ad essa collegati. 
+                        <br>Non una visita guidata del tipo consueto, per raccontare vicende storiche e ammirare l'architettura della rocca millenaria, ma un vero e proprio viaggio per conoscerne il lato più misterioso e oscuro. 
+                        <br>Un percorso che guida all'interno di ogni sala del castello in uno spettacolo itinerante: bisogna prestare attenzione perché dietro ogni angolo potrebbe celarsi una sorpresa... 
+                    </p>
+                    <blockquote>
+                        Una fredda notte di ottobre di tanti secoli fa.
+                        <br>Un avvelenamento che è rimasto nella storia,
+                        <br>ma che è entrato anche nella leggenda.
+                        <br>Una serie di eventi inspiegabili.
+                        <br>Un viaggio per scoprirli..."
+                    </blockquote>
+                    <div class="horizontal-center">
+                        <a href="https://vimeo.com/211328498" target="_blank" class="button button-main">Guarda il trailer video</a>
+                    </div>
+                </div>
+                <figure class="">
+                    <img src="../src/img/foto/2019-misteri.jpg" alt="" class="img-responsive photo">
+                    <figcaption></figcaption>
+                </figure>
+            </section>
+
+            <section class="main-row main-row-invert" aria-labelledby="#secton-title-2">
+                <figure class="">
+                    <img src="../src/img/foto/.jpg" alt="" class="img-responsive photo">
+                    <figcaption></figcaption>
+                </figure>
+                <div class="card">
+                    <h2 class="card-title" id="section-title-2"></h2>
+                    <p>
+                    </p>                                      
+                </div>
+            </section>
+
+            <section class="main-row" aria-labelledby="#secton-title-3">
+                <div class="card">
+                    <h2 class="card-title" id="secton-title-3">"I Misteri del Castello"</h2>
+                    <p>
+                    </p>
+                    
+                </div>
+                <figure class="">
+                    <img src="../src/img/foto/2019-misteri.jpg" alt="" class="img-responsive photo">
+                </figure>
+            </section>
+
+                        
+            <?php require_once '../include/shape-divider.php';?>
+
+        </main>
+
+        <?php require_once 'include/footer.php';?>
+
+    </div>
+
+    <!-- <?php require_once 'include/footer.php';?> -->
+
+</body>
+</html>
