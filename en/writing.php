074030c4338028c71cc69ae3996f83e6fd3d597c
rici86 <rici86@yandex.com>
2022-03-30 18:12:38 +0200

    inizio traduzione scrittura

---
+++ en/writing.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>A passion for writing | Rici86: Coder, Crafter, Creative </title>
+
+    <!-- Meta tags  -->
+    <meta name="description" content="Front-end developer as a living, crafter and Irish dance nerd for passion" />
+	
+    <meta property="article:author" content="https://www.facebook.com/rici86" />
+    <meta property="og:title" content="Rici86 - Coder, Crafter, Creative" />
+    <meta property="og:description" content="Front-end developer as a living, crafter and Irish dance nerd for passion" />
+    <meta property="og:site_name" content="Rici86" />
+    <meta property="og:image" content="../src/img/spirit-of-the-forest.jpg" />
+	
+	<link rel="shortcut icon" href="../src/img/logofavicon.png" type="imag/png" />
+
+    <!-- slick slider  -->
+    <!-- <link rel="stylesheet" type="text/css" href="../src/slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="../src/slick/slick-theme.css"/> -->
+
+    <!-- Css  -->
+    <link href="../src/css/main.css?v=1.11" rel="stylesheet">
+    <link href="../src/fontawesome/css/all.css" rel="stylesheet">
+    
+    <?php require_once '../include/analytics.php';?>
+</head>
+
+<body>
+    <div id="main-container">
+        <?php require_once 'include/nav.php';?>
+
+        <a href="../it/scrittura.php" title="Italiano" class="lang-switcher">
+            <img src="../src/img/ita.png" alt="Italiano" class="lang">
+        </a>
+
+        <main class="with-shape-divider">
+        <section class="main-row" aria-labelledby="#secton-title-1">
+                <div class="card">
+                    <h1 class="card-title" id="secton-title-1">A passion for writing since childhood</h1>
+                    <p>
+                        I wanted to learn very soon how to interpret those signs we call letters, and <a href="https://rici86.blogspot.com/2014/11/storia-di-un-pesce-che-va-scoprire-il.html">since I began writing</a>, I never quit. 
+                        <br>I love the feeling when a blank page comes to life with words, both using a pen or a keboard. 
+                        <br>I always loved writing, and drawing also, setting my fantasy free on paper.
+                    </p>
+                    <p>
+                        I had also the opportunity of publishing something.
+                        <br>Some of my works are scientific papers about archaeology or museums.
+                        <br>Others, are literary works.
+                    </p>                    
+                    <p>
+                        Now I also unleash yy passion for writing on the web, mainly in my <a href="https://www.lrcrafts.it">#LRCrafts blog</a>, dedicated to my family's crafts. 
+                        <br>There's also <a href="https://rici86.blogspot.com">my personal blog</a>, repository of random thoughts for more than ten years.
+                    </p>
+                    <p>
+                        Writing is a passion that requires time, calm and the right state of mind. It doesn't always fit in the lifestyle I've chosen, full of interests. 
+                        <br>It remains an important part of my everyday life, though. 
+                    </p>
+                    <blockquote>
+                        Writing is travelling without the inconvenience of luggages.
+                        <br>(Emilio Salgari)</i>
+                    </blockquote>
+                </div>
+                <figure class="">
+                    <img src="../src/img/foto/2014-presentazionelibro.jpg" alt="" class="img-responsive photo">
+                    <figcaption>The presentation of the book "Misteri e manieri d'Oltrepò", with one of my tales</figcaption>
+                </figure>
+            </section>
+
+            <?php require_once '../include/shape-divider.php';?>
+
+        </main>
+
+        <?php require_once 'include/footer.php';?>
+        <?php require_once '../include/torna-su.php';?>
+
+    </div>
+
+</body>
+</html>
