8bea5eab83810bc55a542f4dddf7d11ccd0ed97e
rici86 <rici86@yandex.com>
2022-03-31 14:37:15 +0200

    traduzione misteri

---
+++ en/mysteries-castle.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>The Misteries of the Castle: a mysterious voyage inside the Dal Verme Castle of Zavattarello | Rici86: Coder, Crafter, Creative </title>
+
+    <!-- Meta tags  -->
+    <meta name="description" content="Front-end developer as a living, crafter and Irish dance nerd for passion" />
+	
+    <meta property="article:author" content="https://www.facebook.com/rici86" />
+    <meta property="og:title" content="Rici86 - Coder, Crafter, Creative" />
+    <meta property="og:description" content="Front-end developer as a living, crafter and Irish dance nerd for passion" />
+    <meta property="og:site_name" content="Rici86" />
+    <meta property="og:image" content="../src/img/spirit-of-the-forest.jpg" />
+	
+	<link rel="shortcut icon" href="../src/img/logofavicon.png" type="imag/png" />
+
+    <!-- slick slider  -->
+    <link rel="stylesheet" type="text/css" href="../src/slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="../src/slick/slick-theme.css"/>
+
+    <!-- Css  -->
+    <link href="../src/css/main.css?v=1.11" rel="stylesheet">
+    <link href="../src/fontawesome/css/all.css" rel="stylesheet">
+    
+    <?php require_once '../include/analytics.php';?>
+</head>
+
+<body>
+    <div id="main-container">
+        <?php require_once 'include/nav.php';?>
+
+        <a href="../it/misteri-del-castello.php" title="Italiano" class="lang-switcher">
+            <img src="../src/img/ita.png" alt="Italiano" class="lang">
+        </a>
+
+        <main class="with-shape-divider" id="main-content">
+            <section class="main-row" aria-labelledby="#secton-title-1">
+                <div class="card">
+                    <h1 class="card-title" id="secton-title-1">The Mysteries of the Castle</h1>
+                    <p>
+                        From legends to weird happenings, through the investigations of paranormal researchers, for one night the visitor can dive into the mysteries of the Castle of Zavattarello, linked to a famous murder.
+                    </p>
+                    <p>
+                        They tell that Pietro Dal Verme, poisoned by his second wife Chiara Sforza on 17th October 1485, is the presence that haunts the rooms of the ancient fortress.
+                        <br>Several inexplicable facts happened inside the castle during its long history: the paranormal researchers began their scientific investigations to shed light on those events. 
+                    </p>
+                    <p>
+                        During this unique night, we guide the visitors through the special <i>voyage into mystery</i>, to discover the places where legend developed and weird facts happened. 
+                    </p>
+                    <p>
+                        It's not a traditional guided tour through the castle, to tell history and appreciate the architecture of the ancient fortress, but a proper voyage to unveil its dark side.
+                        <br>That's a path leading the visitor to each room following a wandering show: everyone must be cautious because behind every corner a surprise might be waiting... 
+                    </p>
+                    
+                    <div class="horizontal-center">
+                        <a href="https://vimeo.com/211328498" target="_blank" class="button button-main">Watch the video trailer</a>
+                    </div>
+                </div>
+                <div class="">
+                    <div class="slider-misteri">
+                        <div class="box-sito">
+                            <img src="../src/img/misteri/misteri01.jpg" alt="" class="img-responsive">      
+                            <div class="sito-description no-flex">            
+                                <p class="sito-url">L'atmosfera che accoglie il visitatore all'ingresso dell'evento</p>
+                                <p class="sito-date">Photo by Angelo Cannella</p>            
+                            </div>  
+                        </div>
+                        <div class="box-sito">
+                            <img src="../src/img/misteri/misteri02.jpg" alt="" class="img-responsive"> 
+                            <div class="sito-description no-flex">
+                                <p class="sito-url">Chiara Sforza trama vendetta</p>
+                                <p class="sito-date">Photo by Angelo Cannella</p>
+                            </div>        
+                        </div>
+                        <div class="box-sito">
+                            <img src="../src/img/misteri/misteri03.jpg" alt="" class="img-responsive">        
+                            <div class="sito-description no-flex">
+                                <p class="sito-url">Musica dal vivo con l'arpa di Claudia Murachelli</p>
+                                <p class="sito-date">Photo by Angelo Cannella</p>            
+                            </div> 
+                        </div>
+                        <div class="box-sito">
+                            <img src="../src/img/misteri/misteri04.jpg" alt="" class="img-responsive">   
+                            <div class="sito-description no-flex">
+                                <p class="sito-url">Pietro Dal Verme morì avvelenato il 17 ottobre 1485</p>
+                                <p class="sito-date">Photo by Angelo Cannella</p>            
+                            </div>      
+                        </div>
+                        <div class="box-sito">
+                            <img src="../src/img/misteri/misteri05.jpg" alt="" class="img-responsive">     
+                            <div class="sito-description no-flex">
+                                <p class="sito-url">Le celle medievali non erano certo luoghi piacevoli...</p>
+                                <p class="sito-date">Photo by Angelo Cannella</p>            
+                            </div>    
+                        </div>
+                        <div class="box-sito">
+                            <img src="../src/img/misteri/misteri06.jpg" alt="" class="img-responsive">     
+                            <div class="sito-description no-flex">
+                                <p class="sito-url">Una lettera annuncia alla corte di Milano la morte di Pietro Dal Verme</p>
+                                <p class="sito-date">Photo by Angelo Cannella</p>            
+                            </div>    
+                        </div>
+                        <div class="box-sito">
+                            <img src="../src/img/misteri/misteri07.jpg" alt="" class="img-responsive">   
+                            <div class="sito-description no-flex">
+                                <p class="sito-url">Tredici edizioni dal 2013 al 2019, ogni serata un sold-out</p>
+                                <p class="sito-date">Photo by Angelo Cannella</p>            
+                            </div> 
+                        </div>
+                        <div class="box-sito">
+                            <img src="../src/img/misteri/misteri08.jpg" alt="" class="img-responsive"> 
+                            <div class="sito-description no-flex">
+                                <p class="sito-url">Il viaggio giunge anche ai nostri giorni e racconta le indagini paranormali svoltesi realmente nel Castello di Zavattarello</p>
+                                <p class="sito-date">Photo by Angelo Cannella</p>            
+                            </div>       
+                        </div>
+                        <div class="box-sito">
+                            <img src="../src/img/misteri/misteri09.jpg" alt="" class="img-responsive">        
+                            <div class="sito-description no-flex">
+                                <p class="sito-url">Il Cavaliere Senza Tempo accompagna i viaggiatori nel loro passaggio dal XV secolo ai giorni nostri</p>
+                                <p class="sito-date">Photo by Angelo Cannella</p>            
+                            </div>
+                        </div>
+                        <div class="box-sito">
+                            <img src="../src/img/misteri/misteri10.jpg" alt="" class="img-responsive"> 
+                            <div class="sito-description no-flex">
+                                <p class="sito-url">Il personaggio più oscuro e misterioso di tutti: il fantasma di Pietro Dal Verme</p>
+                                <p class="sito-date">Photo by Angelo Cannella</p>            
+                            </div>       
+                        </div>
+                        <div class="box-sito">
+                            <img src="../src/img/misteri/misteri11.jpg" alt="" class="img-responsive">        
+                            <div class="sito-description no-flex">
+                                <p class="sito-url">Prigionieri, eretici, dannati o pazzi: al pubblico l'interpretazione</p>
+                                <p class="sito-date">Photo by Angelo Cannella</p>            
+                            </div>
+                        </div>
+                        <div class="box-sito">
+                            <img src="../src/img/misteri/misteri12.jpg" alt="" class="img-responsive">        
+                            <div class="sito-description no-flex">
+                                <p class="sito-url">Chiara Sforza organizza l'avvelenamento del marito Pietro Dal Verme</p>
+                                <p class="sito-date">Photo by Angelo Cannella</p>            
+                            </div>
+                        </div>
+                        <div class="box-sito">
+                            <img src="../src/img/misteri/misteri13.jpg" alt="" class="img-responsive"> 
+                            <div class="sito-description no-flex">
+                                <p class="sito-url">La ricercatrice del paranormale racconta l'episodio misterioso più famoso realmente accaduto: la storia della sedia spostata</p>
+                                <p class="sito-date">Photo by Angelo Cannella</p>            
+                            </div>       
+                        </div>
+                        <div class="box-sito">
+                            <img src="../src/img/misteri/misteri14.jpg" alt="" class="img-responsive">        
+                            <div class="sito-description no-flex">
+                                <p class="sito-url">L'araldo conduce il pubblico lungo il percorso</p>
+                                <p class="sito-date">Photo by Angelo Cannella</p>            
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <?php require_once '../include/shape-divider.php';?>
+
+        </main>
+        
+       <!-- slick slider  -->
+        <script type="text/javascript" src="../src/jquery-3.6.0.slim.min.js"></script>
+        <script type="text/javascript" src="../src/slick/slick.min.js"></script>
+        <script>
+            $('.slider-misteri').slick({
+                rows: 1,
+                infinite: true,
+                autoplay: false,
+                arrows: true,
+                dots: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                swipe: true,
+                swipeToSlide: true,
+                pauseOnHover: true,
+                lazyLoad: 'ondemand'
+            });
+        </script>
+
+        <?php require_once 'include/footer.php';?>
+        <?php require_once '../include/torna-su.php';?>
+
+    </div>
+
+</body>
+</html>
