d63c2f1176fb6168ad22eee2615e5a6c351f77b8
rici86 <rici86@yandex.com>
2022-04-20 14:04:57 +0200

    social: open in new window

--- include/social-icons.php
+++ include/social-icons.php
@@ -1,32 +1,32 @@
 <div class="social">
-    <a href="https://www.linkedin.com/in/rossisara/" title="LinkedIn">
+    <a href="https://www.linkedin.com/in/rossisara/" target="_blank" title="LinkedIn">
         <i class="fab fa-linkedin"></i>
     </a>
-    <a href="https://twitter.com/riciNiSaorsa" title="Twitter">
+    <a href="https://twitter.com/riciNiSaorsa" target="_blank" title="Twitter">
         <i class="fab fa-twitter"></i>
     </a>
-    <a href="https://codepen.io/rici86" title="CodePen">
+    <a href="https://codepen.io/rici86" target="_blank" title="CodePen">
         <i class="fab fa-codepen"></i>
     </a>                
-    <a href="https://github.com/rici86" title="GitHub">
+    <a href="https://github.com/rici86" target="_blank" title="GitHub">
         <i class="fab fa-github"></i>
     </a>                
-    <a href="https://www.instagram.com/ricinisaorsa/" title="Instagram">
+    <a href="https://www.instagram.com/ricinisaorsa/" target="_blank" title="Instagram">
         <i class="fab fa-instagram"></i>
     </a>
-    <a href="https://pinterest.it/rici86/" title="Pinterest">
+    <a href="https://pinterest.it/rici86/" target="_blank" title="Pinterest">
         <i class="fab fa-pinterest"></i>
     </a>
-    <a href="https://vimeo.com/rici86" title="Vimeo">
+    <a href="https://vimeo.com/rici86" target="_blank" title="Vimeo">
         <i class="fab fa-vimeo"></i>
     </a>
-    <a href="https://youtube.com/c/rici86" title="YouTube">
+    <a href="https://youtube.com/c/rici86" target="_blank" title="YouTube">
         <i class="fab fa-youtube"></i>
     </a>
-    <a href="http://rici86.blogspot.it/" title="Blog">
+    <a href="http://rici86.blogspot.it/" target="_blank" title="Blog">
         <i class="fab fa-blogger-b"></i>
     </a>
-    <a href="https://www.lrcrafts.it" title="#LRCrafts">
+    <a href="https://www.lrcrafts.it" target="_blank" title="#LRCrafts">
         <i class="fas fa-hashtag"></i>
     </a>
 </div>
