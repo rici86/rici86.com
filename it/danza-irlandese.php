988a745ae269d39574f77d3a9c9891784264d568
rici86 <rici86@yandex.com>
2022-05-04 18:07:48 +0200

    new page danza

---
+++ it/danza-irlandese.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Danza irlandese e dintorni | Rici86: Coder, Crafter, Creative e anche un po' Nerd </title>
+
+    <!-- Meta tags  -->
+    <meta name="description" content="Front-end developer per lavoro, crafter e Irish dance nerd per passione" />
+	
+    <meta property="article:author" content="https://www.facebook.com/rici86" />
+    <meta property="og:title" content="Rici86 - Coder, Crafter, Creative e anche un po' Nerd" />
+    <meta property="og:description" content="Front-end developer per lavoro, crafter e Irish dance nerd per passione" />
+    <meta property="og:site_name" content="Rici86" />
+    <meta property="og:image" content="../src/img/spirit-of-the-forest.jpg" />
+	
+	<link rel="shortcut icon" href="../src/img/logofavicon.png" type="imag/png" />
+
+    <!-- slick slider  -->
+    <!-- <link rel="stylesheet" type="text/css" href="../src/slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="../src/slick/slick-theme.css"/> -->
+
+    <!-- Css  -->
+    <link href="../src/css/main.css?v=1.11" rel="stylesheet">
+    <link href="../src/fontawesome/css/all.css" rel="stylesheet">
+
+    <?php require_once '../include/analytics.php';?>
+</head>
+
+<body>
+    <div id="main-container">
+        <?php require_once 'include/nav.php';?>
+
+        <a href="../en/irish-dance.php" title="English" class="lang-switcher">
+            <img src="../src/img/eng.png" alt="English" class="lang">
+        </a>
+
+        <main class="with-shape-divider" id="main-content">
+            <section class="main-row" aria-labelledby="#section-title-1">
+                <div class="card">
+                    <h1 class="card-title" id="section-title-1">Danza irlandese: una passione per tutto ciò che è Irlanda</h1>
+                    <p>
+                        
+                    </p>
+                    <blockquote>
+                        <!-- "Sending an audience home with an excited glow and raising their spirits on the way.
+                        This author went looking for that glow of spontaneous excitement 
+                        and found it in the kind of places we call home.
+                        And when everything comes together for that magical moment, we call it the craic." -->
+
+                        "Mandare il pubblico a casa con un bagliore di eccitazione 
+                        <br>e risollevare i loro spiriti lungo la via.
+                        <br>Questo autore è andato alla ricerca di quel bagliore di eccitazione spontanea
+                        <br>e l'ha trovato in quei luoghi che chiamiamo casa.
+                        <br>E quando tutto si riunisce in quel magico momento, noi lo chiamiamo <i>the craic</i>"
+
+                        <br>(Moya Brennan, prefazione a "In search of the craic" di Colin Irwin)
+                    </blockquote>
+
+                </div>
+                <figure class="">
+                    <img src="../src/img/foto/2018-dance-show.jpg" alt="Esibizione a Busto Arsizio (Va) con il corso di danza irlandese Solo Intermediate Gens d'Ys" class="img-responsive photo">
+                    <figcaption>Esibizione a Busto Arsizio (Va) con il corso di danza irlandese Solo Intermediate Gens d'Ys - Foto di Marco Brigandi</figcaption>
+                </figure>
+            </section>
+                        
+            <?php require_once '../include/shape-divider.php';?>
+
+        </main>
+
+        <?php require_once 'include/footer.php';?>
+
+    </div>
+
+</body>
+</html>
